e86a79949fc2719494eed991df1ec67f681948b0
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-19 13:25:40 +0200

    Job1 réalisé

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -16,6 +16,19 @@ echo $str2 ." ". $str . " ". $str3;
 // Affichez le contenu de la variable. Ajoutez 4 à cette variable. Affichez à nouveau
 // le contenu.
 
+$val = 6 ;
+echo $val . " <br> ";
 
 
+$val = $val + 4 ;
+echo $val ."<br>";
+
+// Créez une variable myBool, affectez à cette variable true, affichez le contenu de
+// la variable. Affectez false à la variable et affichez à nouveau le contenu
+
+$myBool = (true);
+var_dump($myBool);
+
+$myBool = (false);
+var_dump($myBool);
 ?>
