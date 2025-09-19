1a500e59e6042a906e1323a31f4bf3e543120adc
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-19 13:32:29 +0200

    ajout des commentaires en une seule ligne et en bloc

---
+++ jour01/job02/index.php
@@ -0,0 +1,17 @@
+<?php
+// ● Dans le dossier runtrack2/jour01/job02, créez un fichier index.php.
+// ● Dans ce fichier, faites afficher deux fois “Hello LaPlateforme!”
+// ● Commentez chacune de ces lignes d’une façon différente (une ligne, un bloc)
+
+
+
+// Ceci est un commentaire en une seule ligne
+echo "Hello LaPlateforme!";
+
+
+/* 
+Ceci est un commentaire en pluseirus lignes
+*/
+echo "Hello LaPlateforme!";
+
+?>
