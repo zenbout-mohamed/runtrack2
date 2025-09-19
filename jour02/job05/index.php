1a80537059bc927294dbb869961eab99f9563fbd
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-19 16:51:19 +0200

    fin du job05

---
+++ jour02/job05/index.php
@@ -0,0 +1,17 @@
+<?php
+/*Faire un algorithme qui affiche les nombres premiers jusqu’à 1000 en mettant un retour
+à la ligne entre chaque nombre (“<br />”). */
+
+for ($i = 2; $i <= 1000; $i++){
+    $premier = true;
+    for ($j = 2; $j <= sqrt($i); $i++){
+        if ($i % $j == 0){
+            $premier = false;
+            break;
+        }
+    }
+    if ($premier){
+        echo $i . "<br />";
+    }
+}
+?>
