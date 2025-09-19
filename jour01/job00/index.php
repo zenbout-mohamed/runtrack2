1e48b8f84f85c344a880b0370139b6e82dfada28
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-19 09:49:42 +0200

    ajout de consignes spécifiques dans chaque fichiers en forme de commentaires

--- jour01/job00/index.php
+++ jour01/job00/index.php
@@ -1,3 +1,5 @@
+<!-- Créez un fichier index.php qui affiche “Hello LaPlateforme!” en php en utilisant
+echo. Ouvrez le avec votre navigateur préféré. -->
 <?php
 // Permet l'affichage du message.
 echo "Hello LaPlateforme!";
---
+++ jour01/job01/index.php
@@ -0,0 +1,7 @@
+<!-- créez une variable nommée str. Affectez lui la valeur
+“LaPlateforme”. Affichez le contenu de la variable str. Créez une variable str2
+contenant “Vive” et une variable str3 contenant “!”. -->
+<?php
+$str ="LaPlateforme";
+echo $str; .
+?>
