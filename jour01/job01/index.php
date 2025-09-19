73fdb76f4969b761fb67c029f4895ae333cd4989
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-19 09:58:45 +0200

    Variable str affiché avec succès

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,7 +1,21 @@
-<!-- créez une variable nommée str. Affectez lui la valeur
-“LaPlateforme”. Affichez le contenu de la variable str. Créez une variable str2
-contenant “Vive” et une variable str3 contenant “!”. -->
+
 <?php
+// créez une variable nommée str. Affectez lui la valeur
+//“LaPlateforme”. Affichez le contenu de la variable str. Créez une variable str2
+//contenant “Vive” et une variable str3 contenant “!”. 
 $str ="LaPlateforme";
-echo $str; .
+echo $str;
+
+
+$str2 ="Vive";
+$str3 = "!";
+echo $str2 ." ". $str . " ". $str3;
+
+
+// Créez une variable nommée val. Affectez à cette variable la valeur 6.
+// Affichez le contenu de la variable. Ajoutez 4 à cette variable. Affichez à nouveau
+// le contenu.
+
+
+
 ?>
