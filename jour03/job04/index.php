3eca0eb3bbf9726cbcc6c2492f11a690912601cb
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-22 13:51:33 +0200

    job04 réalisé

---
+++ jour03/job04/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Créez une variable de type string nommée $str et affectez y le texte :
+“Dans l'espace, personne ne vous entend crier”.
+Créez un algorithme qui parcourt, compte et affiche le nombre de caractères présents
+dans cette chaîne.
+*/ 
+
+$str ="Dans l'espace, personne ne vous entend crier";
+echo "Nombre de caractères : " . strlen($str);
+
+
+
+?>
