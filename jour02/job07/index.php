4e1c718d9446996ba59f7f1bf68877767a8a7b97
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-22 09:49:23 +0200

    finalisation job07

---
+++ jour02/job07/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*Faire un algorithme qui affiche un triangle de 5 de hauteur. Cette dimension devra être
+stockée dans une variable $hauteur, modifiable facilement.
+
+Exemple si $hauteur = 5 */
+
+
+$hauteur = 10;
+
+for ($i = 1; $i <= $hauteur; $i++){
+    for($j = 1; $j <= $i; $j++){
+        echo "*";
+    }
+    echo "<br>";
+}
+
+?>
