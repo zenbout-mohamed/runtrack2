74a6cc2bc9bd5f3fca96ec3e749e933b6dd9cb05
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-22 15:55:40 +0200

    finalisation job06

---
+++ jour03/job06/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Créez une variable de type string nommée $str et affectez y le texte :
+“Les choses que l'on possède finissent par nous posséder."
+Créez un algorithme qui parcourt et écrit cette chaine à l’envers.
+Ex. : redessop suon rap tnessinif edessop no'l euq sesohc seL
+
+*/ 
+
+$str ="Les chose que l'on possède finissent par nous posséder.";
+
+echo strrev($str);
+?>
