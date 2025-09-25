85ddba3da9aba215d09ff806ece9df391563fe11
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-25 13:57:13 +0200

    job01 réalisé

---
+++ jour07/job01/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Créez une fonction nommée “hello()”. Cette fonction, une fois appelée, doit afficher sur
+la page : “Hello LaPlateforme!”.
+*/
+function hello(){
+
+    echo "Hello LaPlateforme!";
+}
+
+hello();
+
+
+
+?>
