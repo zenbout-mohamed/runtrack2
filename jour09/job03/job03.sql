ec9e80652d07f1da3a80e38ce13cf123371d3a06
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-25 21:59:55 +0200

    job03 réalisé

---
+++ jour09/job03/job03.sql
@@ -0,0 +1,6 @@
+-- Maintenant que vous avez créé une base de données, des tables et que vous y avez
+-- ajouté des données, vous allez pouvoir les manipuler.
+-- Ecrivez dans le fichier “job03.sql” une requête permettant de sélectionner l’ensemble
+-- des champs de la table etudiants.
+
+SELECT * FROM etudiants;
