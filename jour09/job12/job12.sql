ce526d4ca6aedc737d8c5dbae7f9e3a7570fd366
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-26 09:40:49 +0200

    job12 réalisé

---
+++ jour09/job12/job12.sql
@@ -0,0 +1,5 @@
+-- Ecrivez dans le fichier “job12.sql” une requête permettant de sélectionner l’ensemble
+-- des salles en les triant par capacité décroissante.
+
+SELECT * FROM salles
+ORDER BY capacite DESC;
